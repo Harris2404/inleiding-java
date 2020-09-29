34e16d0e858b579922a2a267c85cabeb3a3ac993
Harris2404 <62289266+Harris2404@users.noreply.github.com>
2020-09-29 13:05:13 +0200

    h2 refresh

---
+++ src/h02/src/opdracht2.java
@@ -0,0 +1,22 @@
+package HO2;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdracht2 extends Applet {
+    @Override
+    public void init() {
+        super.init();
+        setBackground(Color.white);
+    }
+
+    @Override
+    public void paint (Graphics g) {
+        super.paint(g);
+        g.setColor(Color.blue);
+        g.drawString("Harris", 50, 60);
+        super.paint(g);
+        g.setColor(Color.red);
+        g.drawString("Doeve", 70, 60);
+    }
+}
